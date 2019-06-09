87e2db8a0e1b08357d5cbe98f593bc2bb0b383d3
kiko8994 <89stanac@gmail.com>
2019-06-09 10:27:27 +0200

    NN

--- app/src/main/java/com/example/turnirmk/MyTournament.java
+++ app/src/main/java/com/example/turnirmk/MyTournament.java
@@ -1,13 +1,16 @@
 package com.example.turnirmk;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
